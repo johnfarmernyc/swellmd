5577e3962f42c95b204265450c0f104b1f2b4cf4
John Farmer <johnfarmernyc@gmail.com>
2014-06-29 13:03:17 -0400

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swellmd::Application.config.secret_key_base = 'b4b708ffe22dfb3b6fcca6cd7ede9353a981a85376b1bebe01946de1e53d780229acea28e92f10e5fb27553ebe910abd14a4df0ade13175a59c6591f1ca156c0'
